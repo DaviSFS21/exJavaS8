7face92c163a461dff93bde5a171191bd1b842fe
Davi Soares <davisfs2110@gmail.com>
2024-05-21 21:19:10 -0300

    refactor: Muda nome das variáveis

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -1,11 +1,11 @@
 package entities;
 
 public class Employee {
-    public String Name;
-    public double GrossSalary;
-    public double Tax;
+    public String name;
+    public double grossSalary;
+    public double tax;
     public double percentage;
 
-    public double NetSalary() { return GrossSalary - Tax; }
-    public double IncreaseSalary() { return (GrossSalary - Tax) + (GrossSalary * (percentage / 100)); }
+    public double NetSalary() { return grossSalary - tax; }
+    public double IncreaseSalary() { return (grossSalary - tax) + (grossSalary * (percentage / 100)); }
 }
