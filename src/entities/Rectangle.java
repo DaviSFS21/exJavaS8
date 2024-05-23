f61fb0fd58b914aae806fd1c5312e494c7ef7b25
Davi Soares <davisfs2110@gmail.com>
2024-05-23 20:37:08 -0300

    refactor: Muda o nome das variáveis

--- src/entities/Rectangle.java
+++ src/entities/Rectangle.java
@@ -1,11 +1,11 @@
 package entities;
 
 public class Rectangle {
-    public double a;
-    public double b;
+    public double width;
+    public double height;
 
-    public double area() { return a * b; }
-    public double perimeter() { return a * 2 + b * 2; }
-    public double diagonal() { return Math.sqrt(Math.pow(a,2) + Math.pow(b,2)); }
+    public double area() { return width * height; }
+    public double perimeter() { return width * 2 + height * 2; }
+    public double diagonal() { return Math.sqrt(Math.pow(width,2) + Math.pow(height,2)); }
 
 }
